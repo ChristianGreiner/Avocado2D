bfa0724d8087e60c2a13c3ce9c1e0fda8325a46d
christian.greiner <christian.greiner97@gmx.de>
2016-08-04 10:34:34 +0200

    Added Color-Converter

---
+++ Avocado2D/Avocado2D/Graphics/ColorConverter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+
+namespace Avocado2D.Graphics
+{
+    public static class ColorConverter
+    {
+        /// <summary>
+        /// Converts a hexcode to a color.
+        /// </summary>
+        /// <param name="hexcode">The hexcode.</param>
+        /// <returns>Returns the color.</returns>
+        public static Color HexToColor(string hexcode)
+        {
+            return new Color(
+                             Convert.ToInt32(hexcode.Substring(1, 2), 16),
+                             Convert.ToInt32(hexcode.Substring(3, 2), 16),
+                             Convert.ToInt32(hexcode.Substring(5, 2), 16));
+        }
+    }
+}
